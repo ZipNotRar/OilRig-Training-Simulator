3f50ac9d2302055b95a676932e0eb37b5260985e
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2021-11-14 21:30:25 +0100

    Transitioning from old project to new
    1. Lever and buttons are implemented.
    2. Extinguisher grabbing and textures on it are also enabled.
    3. I think I found a way to use only one controller for movement + rotation! (Fingers crossed if it works)

---
+++ Assets/Scripts/LeverEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class LeverEvents : MonoBehaviour
+{
+    [SerializeField] private HingeJoint leverJoint;
+    public UnityEvent onPulled, onPushed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leverJoint = GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.rotation.x < 10)
+        {
+            onPulled.Invoke();
+        }
+        if (transform.rotation.x > 0)
+        {
+            onPushed.Invoke();
+        }
+    }
+
+
+}
