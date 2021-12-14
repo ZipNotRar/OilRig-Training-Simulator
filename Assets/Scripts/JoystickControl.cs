c016a15d30ab441aee877332215a8cb1326c250f
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2021-12-14 13:40:43 +0100

    Pushing development changes
    1. Lever's working in X position.
    2. Movement working via hand.

--- Assets/Scripts/JoystickControl.cs
+++ Assets/Scripts/JoystickControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class JoystickControl : MonoBehaviour
 {
@@ -8,44 +9,48 @@ public class JoystickControl : MonoBehaviour
     [SerializeField]
     private float forwardBackWardTilt = 0;
     [SerializeField]
-    private float sideTosideTilt = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    //private float sideTosideTilt = 0;
+    public UnityEvent onForwardTilt;
+    public UnityEvent onBackwardTilt;
+    public Transform l_hand;
+    public Transform r_hand;
     // Update is called once per frame
+    void Start() { 
+    }
     void Update()
     {
         forwardBackWardTilt = topOfJoystick.rotation.eulerAngles.x;
-        if(forwardBackWardTilt < 355 && forwardBackWardTilt > 290)
+        if(forwardBackWardTilt < 345 && forwardBackWardTilt > 280)
         {
             forwardBackWardTilt = Mathf.Abs(forwardBackWardTilt - 360);
+            onBackwardTilt.Invoke();
             Debug.Log("Backward" + forwardBackWardTilt);
         }
-        else if( forwardBackWardTilt > 5 && forwardBackWardTilt < 74)
+        else if( forwardBackWardTilt > 15 && forwardBackWardTilt < 80)
         {
+            onForwardTilt.Invoke();
             Debug.Log("Forward" + forwardBackWardTilt);
         }
 
-        sideTosideTilt = topOfJoystick.rotation.eulerAngles.z;
-        if(sideTosideTilt < 355 && sideTosideTilt > 290)
-        {
-            sideTosideTilt = Mathf.Abs(sideTosideTilt - 360);
-            Debug.Log("Right" + sideTosideTilt);
-        }
-        else if(sideTosideTilt > 5 && sideTosideTilt < 74)
-        {
-            Debug.Log("Left" + sideTosideTilt);
-        }
+        // sideTosideTilt = topOfJoystick.rotation.eulerAngles.z;
+        // if(sideTosideTilt < 355 && sideTosideTilt > 290)
+        // {
+        //     sideTosideTilt = Mathf.Abs(sideTosideTilt - 360);
+        //     Debug.Log("Right" + sideTosideTilt);
+        // }
+        // else if(sideTosideTilt > 5 && sideTosideTilt < 74)
+        // {
+        //     Debug.Log("Left" + sideTosideTilt);
+        // }
     }
 
     private void OnTriggerStay(Collider other)
     {
         if(other.GetComponentInParent<SphereCollider>())
         {
-            transform.LookAt(other.transform.position, transform.up);
+            transform.LookAt(new Vector3(l_hand.position.x ,l_hand.position.y, transform.position.z), transform.up);
+            transform.LookAt(new Vector3(r_hand.position.x, r_hand.position.y, transform.position.z), transform.up);
+            //transform.LookAt(other.transform.position, transform.up);
         }
     }
 }
