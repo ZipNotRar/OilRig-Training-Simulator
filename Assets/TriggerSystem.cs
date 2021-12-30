188fa5897623b8d563de39bc73d08285b06c4127
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2021-12-30 09:39:44 +0100

    Trigger mechanics for control room's door
    1. Attempt at making the stopwatch triggered using the door itself.

---
+++ Assets/TriggerSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+public class TriggerSystem : MonoBehaviour
+{
+    public Timer timer;
+    private void OnTriggerStay(Collider other) {
+        if (other.GetComponent<OVRPlayerController>())
+        {
+            timer.StartTimer();
+            GameObject timerTriggerEnter = GameObject.Find("TimerTriggerEnter");
+            Destroy(timerTriggerEnter);
+        }
+    }
+}
