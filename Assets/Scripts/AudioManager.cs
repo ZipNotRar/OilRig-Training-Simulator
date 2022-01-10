d1370de11cbd008082f5f00090b4c347fad68e65
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2022-01-10 19:20:10 +0100

    Developing the first stage of Narrative design
    1. Control panel is organized.
    2. Animation is placed in the first stage.
    3. User interactions are added to the first stage.
    4.Possible fix to import: 2 APKs with heavy load were left from the previous updates. Removed them.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public bool rw1 = false;
+    public AudioSource as1;
+
+    private void OnTriggerStay(Collider other) {
+        if (other.name == "Cubo")
+        {
+            rw1 = true;
+            if (rw1 == true)
+            {
+                Debug.Log("play recording now");
+                as1.Play();
+            }
+        }
+
+    }
+}
