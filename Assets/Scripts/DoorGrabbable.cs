1282e336b5c719b3a46467109fdb1a4a0cc44d1a
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2022-01-24 22:11:12 +0100

    Bug fixing
    1. Door is made to be more responsive.
    2. Physics have been applied to door movement.

---
+++ Assets/Scripts/DoorGrabbable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorGrabbable : OVRGrabbable
+{
+    public Transform handler;
+    public float maxGrabDistance;
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(Vector3.zero, Vector3.zero);
+
+        transform.position = handler.transform.position;
+        transform.rotation = handler.transform.rotation;
+
+        Rigidbody rbHandler = handler.GetComponent<Rigidbody>();
+        rbHandler.velocity = Vector3.zero;
+        rbHandler.angularVelocity = Vector3.zero;
+    }
+    private void Update()
+    {
+        if (Vector3.Distance(handler.position, transform.position) > maxGrabDistance)
+        {
+            grabbedBy.ForceRelease(this);
+        }
+    }
+}
