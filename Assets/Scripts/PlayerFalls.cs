3f5d08f1b1e909cd7617a8e6bc81e9c07b264eea
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2022-02-02 13:26:33 +0100

    Fall prevention
    1. Colliders added around the platform for user to be blocked from falling OFF the map.
    2. User can collider with exterior objects. Mesh colliders are implemented for this.

---
+++ Assets/Scripts/PlayerFalls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFalls : MonoBehaviour
+{
+    public Transform fallThreshold;
+    public Transform spawnPoint;
+
+    private void Update()
+    {
+        if (this.transform.position.y < fallThreshold.transform.position.y)
+        {
+            this.transform.position = spawnPoint.transform.position;
+        }
+    }
+}
